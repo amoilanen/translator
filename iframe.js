c00baf6788d6b527b4958ae07415082dfe122c7d
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-12-30 22:38:42 +0200

    Translate the selected word

--- iframe.js
+++ iframe.js
@@ -5,11 +5,32 @@ const checkForResultInterval = setInterval(() => {
   if (translationElement) {
     const translation = translationElement.innerText;
     console.log("translation in iFrame = ", translation);
-    const message = {
-      type: 'translation:ready',
-      payload: translation
-    };
-    window.parent.postMessage(JSON.stringify(message), '*');
+    sendTranslationToParent(translation);
     clearInterval(checkForResultInterval);
   }
 }, 500);
+
+function sendTranslationToParent(translation) {
+  const message = {
+    type: 'translation:ready',
+    payload: translation
+  };
+  window.parent.postMessage(JSON.stringify(message), '*');
+}
+
+window.addEventListener('message', event => {
+  const message = JSON.parse(event.data);
+  if (message.type === 'translation:request') {
+    const toTranslate = message.payload;
+
+    const checkForResultInterval = setInterval(() => {
+        const translationElement = document.querySelector('.tlid-translation.translation');
+        if (translationElement) {
+          const translation = translationElement.innerText;
+          console.log("translation in iFrame = ", translation);
+          sendTranslationToParent(translation);
+          clearInterval(checkForResultInterval);
+        }
+      }, 500);
+  }
+}, false);
--- translate.js
+++ translate.js
@@ -1,7 +1,12 @@
-let url = "https://translate.google.com/#view=home&op=translate&sl=fi&tl=en&text=valo";
+function constructFrameUrl(word) {
+  return `https://translate.google.com/#view=home&op=translate&sl=fi&tl=en&text=${word}`;
+}
+
+let url = constructFrameUrl('valo');
 
 let iframe = document.createElement('iframe');
 iframe.src = url;
+iframe.setAttribute('name', 'translation-frame')
 iframe.style.display = 'none';
 
 document.body.appendChild(iframe);
@@ -18,3 +23,22 @@ window.addEventListener('message', event => {
     console.log(error);
   }
 }, false);
+
+document.addEventListener('selectionchange', event => {
+  const selectedText = document.getSelection().toString();
+
+  //console.log('Selected text:');
+  //console.log(selectedText);
+
+  if (selectedText.length > 0) {
+    const iframe = document.querySelector('iframe[name=translation-frame]');
+    iframe.setAttribute('src', constructFrameUrl(selectedText));
+
+    const message = {
+      type: 'translation:request',
+      payload: selectedText
+    };
+    console.log(`Asking to translate "${selectedText}"`);
+    iframe.contentWindow.postMessage(JSON.stringify(message), '*');
+  }
+});
