d4cde6318b140a736dcb7acae0129c29fedd15ff
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-02-10 23:34:32 +0200

    Support switching language for YandexFrame

--- src/iframe/YandexFrame.js
+++ src/iframe/YandexFrame.js
@@ -1,4 +1,24 @@
+import { sendMessage, Messages } from '../Messages.js';
 import Frame from './Frame.js';
+
+function constructFrameUrl(text, sourceLanguage, targetLanguage) {
+  return `https://translate.yandex.com/?lang=${sourceLanguage}-${targetLanguage}&text=${text}`;
+}
+
+function getCurrentState() {
+  const langParamKeyValue = window.location.search.split('&')[0];
+  const langParamValue = langParamKeyValue.split('=')[1];
+  if (langParamValue) {
+    const [sourceLanguage, targetLanguage] = langParamValue.split('-');
+
+    return {
+      sourceLanguage,
+      targetLanguage
+    };
+  } else {
+    return {};
+  }
+}
 
 function simulateEnter(element) {
   const event = document.createEvent('Events');
@@ -12,7 +32,17 @@ export default class YandexFrame  extends Frame {
 
     constructor() {
       super();
+      this.sourceLanguage = '';
+      this.targetLanguage = '';
       this.translationResultBefore = '';
+    }
+
+    init() {
+      super.init();
+      const translationResult = this.getTranslationResult();
+      if (translationResult != '') {
+        sendMessage(window.parent, Messages.TranslationReady, translationResult);
+      }
     }
 
     get sourceElement() {
@@ -23,10 +53,18 @@ export default class YandexFrame  extends Frame {
       return document.querySelector('#translation');
     }
   
-    startTranslation(sourceText) {
-      this.translationResultBefore = this.resultElement.textContent;
-      this.sourceElement.innerText = sourceText;
-      simulateEnter(this.sourceElement);
+    startTranslation(sourceText, sourceLanguage, targetLanguage) {
+      const {
+        sourceLanguage: currentSourceLanguage,
+        targetLanguage: currentTargetLanguage
+      } = getCurrentState();
+      if (currentSourceLanguage != sourceLanguage || currentTargetLanguage != targetLanguage) {
+        window.location.href = constructFrameUrl(sourceText, sourceLanguage, targetLanguage);
+      } else {
+        this.translationResultBefore = this.resultElement.textContent;
+        this.sourceElement.innerText = sourceText;
+        simulateEnter(this.sourceElement);
+      }
     }
 
     isTranslationInProgress() {
